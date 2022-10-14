d3b3f1db5be07ab8144c1195ce664827e8aa9996
felipeggarcia <91134856+felipegogarcia@users.noreply.github.com>
2022-10-14 15:56:45 -0300

    Revert "alterações para adição de responsividade"
    This reverts commit 35c39d3ce8da9da5788732e5a3e1150546b2701c.

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -14,55 +14,46 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (ctx, constraints) {
-        return Column(
-          children: [
-            SizedBox(
-              height: constraints.maxHeight * 0.15,
-              child: FittedBox(
-                child: Text(value!.toStringAsFixed(2)),
-              ),
-            ),
-            SizedBox(height: constraints.maxHeight * 0.05),
-            SizedBox(
-              height: constraints.maxHeight * 0.6,
-              width: 10,
-              child: Stack(
-                alignment: Alignment.bottomCenter,
-                children: <Widget>[
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey,
-                        width: 1.0,
-                      ),
-                      color: const Color.fromRGBO(220, 220, 220, 1),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
+    return Column(
+      children: [
+        SizedBox(
+          height: 20,
+          child: FittedBox(
+            child: Text('${value!.toStringAsFixed(2)}'),
+          ),
+        ),
+        const SizedBox(height: 5),
+        SizedBox(
+          height: 60,
+          width: 10,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
                   ),
-                  FractionallySizedBox(
-                    heightFactor: percentage,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.primary,
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
+                  color: const Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primary,
+                    borderRadius: BorderRadius.circular(5),
                   ),
-                ],
+                ),
               ),
-            ),
-            SizedBox(height: constraints.maxHeight * 0.05),
-            SizedBox(
-              height: constraints.maxHeight * 0.15,
-              child: FittedBox(
-                child: Text(label!),
-              ),
-            ),
-          ],
-        );
-      },
+            ],
+          ),
+        ),
+        const SizedBox(height: 5),
+        Text(label!),
+      ],
     );
   }
 }
